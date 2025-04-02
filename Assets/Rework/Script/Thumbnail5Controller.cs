84c53b2fd1f2a0a3cdeb7be7f4693f8fd5c9fee8
Jaikishore <jaikishore@dlearners.in>
2025-04-02 22:37:49 +0530

    rework changes done until 7th slide

---
+++ Assets/Rework/Script/Thumbnail5Controller.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Thumbnail5Controller : MonoBehaviour
+{
+    public List<QuestionOptions> questionOptions;
+    public TextMeshProUGUI questionText;
+    public Transform[] optionObjs;
+    public Transform qoDisplayPanel;
+    public Transform questionStartPos, questionEndPos;
+    public Transform counterPanel;
+    public GameObject blockPanel;
+    public GameObject activityCompleted;
+    int currentIndex = 0;
+
+    void Start()
+    {
+        SwitchNextQuestion();
+        UpdateCounter();
+        LowerCounterDisplay();
+        DisableActivityCompleted();
+    }
+
+    private void LowerCounterDisplay()
+    {
+        Utilities.Instance.ANIM_Move(counterPanel, counterPanel.transform.position + (Vector3.down * 2.5f));
+    }
+
+    void EnableBLockPanel() => blockPanel.SetActive(true);
+    void DisableBlockPanel() => blockPanel.SetActive(false);
+    void DisableActivityCompleted() => activityCompleted.SetActive(false);
+    void EnableActivityCompleted() => activityCompleted.SetActive(true);
+
+    void UpdateCounter()
+    {
+        counterPanel.GetComponentInChildren<TextMeshProUGUI>().text = $"{currentIndex + 1}/{questionOptions.Count}";
+    }
+
+    void SwitchNextQuestion()
+    {
+        if(currentIndex == questionOptions.Count) { EnableActivityCompleted(); return; }
+
+        questionText.text = questionOptions[currentIndex].question;
+
+        for (int i = 0; i < optionObjs.Length; i++)
+        {
+            optionObjs[i].GetComponentInChildren<TextMeshProUGUI>().text = questionOptions[currentIndex].options[i];
+        }
+        ResetRightAnswer();
+        UpdateCounter();
+        DisableBlockPanel();
+        Utilities.Instance.ANIM_Move(qoDisplayPanel, new Vector3(0,qoDisplayPanel.position.y,0));
+    }
+
+    void ResetRightAnswer()
+    {
+        foreach (var optObj in optionObjs)
+        {
+            optObj.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
+    void ChangePanel()
+    {
+        Utilities.Instance.ANIM_Move(qoDisplayPanel, new Vector3(questionEndPos.position.x, qoDisplayPanel.position.y, questionEndPos.position.z), 
+        callBack: () => {
+            qoDisplayPanel.transform.position = new Vector3(questionStartPos.position.x, qoDisplayPanel.position.y, questionStartPos.position.z);
+            currentIndex++;
+            SwitchNextQuestion();
+        });
+    }
+
+#region Listeners
+
+    public void OnOptionClickListeners()
+    {
+        var selectedOpt = EventSystem.current.currentSelectedGameObject;
+        var selOptTxt = selectedOpt.GetComponentInChildren<TextMeshProUGUI>().text;
+        if(questionOptions[currentIndex].IsRightAnswer(selOptTxt))
+        {
+            EnableBLockPanel();
+            selectedOpt.transform.GetChild(0).gameObject.SetActive(true);
+            Utilities.Instance.ANIM_CorrectScaleEffect(selectedOpt.transform, callback: ChangePanel);
+        }else{
+            Utilities.Instance.ANIM_WrongShakeEffect(selectedOpt.transform);
+        }
+    }
+    
+#endregion
+
+}
+
+[Serializable]
+public class QuestionOptions
+{
+    public string question;
+    public List<int> rightOptionsIndex;
+    public List<string> options;
+
+    public bool IsRightAnswer(string answerSTR) => rightOptionsIndex.Contains(options.IndexOf(answerSTR));
+}
---
+++ Assets/Rework/Script/Thumbnail7Controller.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Thumbnail7Controller : MonoBehaviour
+{
+    public Transform numberBlockParent;
+    public TextMeshProUGUI upperTens, upperOnes, lowerTens, lowerOnes, tensPower;
+    public Transform counterObj;
+    public string[] _quesitons;
+    public GameObject[] answerObjs;
+    public Transform boardObj;
+    public Transform startPoint, endPoint;
+    public GameObject activityCompleted;
+    int currentIndex = 0;
+    Vector3 boardCurrentPosition;
+    string[] currentQuesSplit;
+
+    void Start()
+    {
+        boardCurrentPosition = boardObj.transform.position;
+        ShowNextQues();
+        UpdateCounter();
+        DisableActivityCompleted();
+    }
+
+    private void OnEnable() {
+        ImageDropSlot.onDropInSlot += OnObjectDrop;
+    }
+
+    bool ShowNextQues()
+    {
+        if(currentIndex == _quesitons.Length) { EnableActivityCompleted(); return false; }
+
+        string currentQues = _quesitons[currentIndex];
+        currentQuesSplit = currentQues.Split('+');
+        upperTens.text = currentQuesSplit[0].Trim()[0].ToString();
+        upperOnes.text = currentQuesSplit[0].Trim()[1].ToString();
+
+        lowerTens.text = currentQuesSplit[1].Trim()[0].ToString();
+        lowerOnes.text = currentQuesSplit[1].Trim()[1].ToString();
+
+        tensPower.text = "";
+        answerObjs[0].GetComponentInChildren<TextMeshProUGUI>().text = "";
+        answerObjs[1].GetComponentInChildren<TextMeshProUGUI>().text = "";
+        return true;
+    }
+
+    void UpdateCounter() => counterObj.GetComponentInChildren<TextMeshProUGUI>().text = $"{currentIndex + 1}/{_quesitons.Length}";
+
+    void OnObjectDrop(GameObject draggedObject, GameObject dropSlotObject)
+    {
+        dropSlotObject.GetComponentInChildren<TextMeshProUGUI>().text = draggedObject.GetComponentInChildren<TextMeshProUGUI>().text;
+
+        var tensValue = answerObjs[0].GetComponentInChildren<TextMeshProUGUI>().text;
+        var onesValue = answerObjs[1].GetComponentInChildren<TextMeshProUGUI>().text;
+
+        bool tensValueCheck = tensValue == "";
+        bool onesValueCheck = onesValue == "";
+
+        if(dropSlotObject.name.Equals("Ones") && (int.Parse(upperOnes.text) + int.Parse(lowerOnes.text) >= 10))
+        {
+            tensPower.text = "+1";
+        }
+
+        if(tensValueCheck || onesValueCheck) return;
+
+        bool rightAnswer = (int.Parse(currentQuesSplit[0].Trim()) + int.Parse(currentQuesSplit[1].Trim())) == int.Parse($"{tensValue}{onesValue}"); 
+
+        if(rightAnswer)
+        {
+            Invoke(nameof(MoveBoardLeft), 1f);
+        }else{
+            Utilities.Instance.ANIM_WrongShakeEffect(answerObjs[0].transform, callback: () => {
+                answerObjs[0].GetComponentInChildren<TextMeshProUGUI>().text = "";
+            });
+            Utilities.Instance.ANIM_WrongShakeEffect(answerObjs[1].transform, callback: () => {
+                answerObjs[1].GetComponentInChildren<TextMeshProUGUI>().text = "";
+                tensPower.text = "";
+            });
+        }
+    }
+
+    void DisableActivityCompleted() => activityCompleted.SetActive(false);
+    void EnableActivityCompleted() => activityCompleted.SetActive(true);
+
+    void MoveBoardLeft()
+    {
+        Utilities.Instance.ANIM_Move(boardObj, new Vector3(endPoint.position.x, boardObj.position.y, endPoint.position.z), callBack: () => {
+            boardObj.transform.position = new Vector3(startPoint.position.x, boardObj.position.y, startPoint.position.z);
+
+            currentIndex++;
+
+            if(ShowNextQues())
+                Utilities.Instance.ANIM_Move(boardObj, new Vector3(boardCurrentPosition.x, boardObj.position.y, boardCurrentPosition.z), callBack : UpdateCounter);
+        });
+    }
+}
